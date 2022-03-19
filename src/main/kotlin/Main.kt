ad573e39aa96b7b8bc32684c9506d58e5396bde5
Konstantin Malcev <maltkost27@gmail.com>
2022-03-19 10:36:17 +0500

    Added feature 111 people

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,13 +3,13 @@ import kotlin.math.roundToInt
 fun main() {
     print("Введи количество лайков:__")
     val likes = readLine()?.toInt()
-    val tail = verification(likes)
-    val people = if (likes!! % 10 == 1 && likes != 11 && tail != 0.11) "человеку" else "людям"
+//    val tail = verification(likes)
+    val people = if (likes!! % 10 == 1 && likes != 11 && likes % 100 == 11) "человеку" else "людям"
 
     println("Ваше фото понравилось $likes $people")
 }
 
-private fun verification(likes: Int?): Double {
-    val recalculation = (likes?.div(100.00))?.minus((likes / 100))
-    return (recalculation?.times(100))!!.roundToInt() / 100.0
-}
+//private fun verification(likes: Int?): Double {
+//    val recalculation = (likes?.div(100.00))?.minus((likes / 100))
+//    return (recalculation?.times(100))!!.roundToInt() / 100.0
+//}
